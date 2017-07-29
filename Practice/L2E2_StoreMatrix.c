e0695c0020613bda5334ceb05d590f335522193a
Sebastian Baker <bakersebastian00@gmail.com>
2017-07-29 13:53:12 +1000

    cleanup in practice code

--- Practice/L2E2_StoreMatrix.c
+++ Practice/L2E2_StoreMatrix.c
@@ -1,5 +1,8 @@
 /*
 Written by Sebastian Baker as practice for Unimelb - Numerical Programming for Engineers
+
+compile: gcc L2E2_StoreMatrix.c -o storeMatrix -Wall -Werror
+test run: ./storeMatrix matrix.txt
 ================================================================================================
 Lecture 2, Ex2
 Write a program that can read n×m matrix of ints from stdin with the following format:
@@ -20,6 +23,10 @@ Sample data ﬁle: matrix.txt
 #include <float.h>
 
 #define READ_ONLY_FILE "r"
+#define MIN(a, b) ( (a < b) ? a : b )
+#define MAX(a, b) ( (a < b) ? b : a )
+#define MIN_FILENAME_LENGTH strlen("a.txt")
+
 
 typedef struct matrixStruct  matrix_t;
 struct matrixStruct {
@@ -28,97 +35,92 @@ struct matrixStruct {
 	double ** data;
 };
 
-void processMatrixFile(char * filename);
-void parseMatrixFile(char * filename, matrix_t *matrix);
-double * parseRow(FILE *fp, int fileRow, int matrixCols);
+matrix_t * parseMatrix(char * filename);
+double ** parseMatrixData(FILE *fp, int totalRows, int totalCols);
 void printMinMaxInEachCol(matrix_t matrix);
 void deleteMatrix(matrix_t * matrix);
 
 int main(int argv, char *argc[]) {
 
-	// get input
 	char *filename = argc[1];
-	if (strlen(argc[1]) <= 4) {
-		fprintf(stderr, "Filename too short! Exiting...\n");
-		exit(EXIT_FAILURE);
-	}
+	assert(strlen(filename) >= MIN_FILENAME_LENGTH);
 	printf("File input: %s\n", filename);
 
-	processMatrixFile(filename);
+	matrix_t * matrix = parseMatrix(filename);
+	printMinMaxInEachCol(*matrix);
+	deleteMatrix(matrix);
+	matrix = NULL;
+
 
 	printf("done!\n");
 	return 0;
 }
 
-void processMatrixFile(char * filename) {
-
-	// initialise matrix
-	matrix_t * matrix = (matrix_t *)(malloc(sizeof(matrix_t)));
-	assert(matrix != NULL);
-	
-	// do stuff with the matrix
-	parseMatrixFile(filename, matrix);
-	printMinMaxInEachCol(*matrix);
-	deleteMatrix(matrix);
-	matrix = NULL;
-}
-
-void parseMatrixFile(char * filename, matrix_t *matrix) {
+matrix_t * parseMatrix(char * filename) {
 
 	// open file
 	FILE * fp = fopen(filename, READ_ONLY_FILE);
 	assert(fp != NULL);
 
-	// parse size
+	// initialise matrix
+	matrix_t * matrix = (matrix_t *)(malloc(sizeof(matrix_t)));
+	assert(matrix != NULL);
+
+	// parse size info
 	assert(fscanf(fp, "n = %d\n", &(matrix->rows)) == 1);
 	assert(fscanf(fp, "m = %d\n", &(matrix->cols)) == 1);
 	printf("rows n = %d, cols m = %d\n", matrix->rows, matrix->cols);
 	
-	// allocate memory space for all the rows
-	int bytesInArrayOfRowPtrs = sizeof(double*)*(matrix->rows);
-	matrix->data = (double**)malloc(bytesInArrayOfRowPtrs);
-	assert(matrix->data != NULL);
-
-	// parse file content by row
-	int fileRow=0;
-	for (fileRow=0; fileRow<matrix->rows; fileRow++) {
-		matrix->data[fileRow] = parseRow(fp, fileRow, matrix->cols);
-	}
+	// parse and allocate content of matrix
+	matrix->data = parseMatrixData(fp, matrix->rows, matrix->cols);
 
 	// close file
 	fclose(fp);
 	fp = NULL;
+	return matrix;
 }
 
-double * parseRow(FILE *fp, int fileRow, int matrixCols) {
+double ** parseMatrixData(FILE *fp, int totalRows, int totalCols) {
 
-	// allocate mem for new row in matrix
-	int bytesInRow = sizeof(double)*(matrixCols);
-	double * newRow = (double*)malloc(bytesInRow);
-	assert(newRow != NULL);
+	// bytes needed for mem alloc
+	int bytesInArrayOfPtrsToRows = sizeof(double*)*(totalRows);
+	int bytesPerRow = sizeof(double)*(totalCols);
 
-	// Read data into the new array
-	int fileCol=0;
-	for (fileCol=0; fileCol<matrixCols; fileCol++) {
-		assert(fscanf(fp," %lf\n", &(newRow[fileCol])) == 1);
-		//printf("Recorded col=%d, row=%d, data=%.0f\n",fileCol, fileRow, newRow[fileCol]);
+	// alloc mem for array of ptrs to rows
+	double ** arrayOfPtrsToRows = (double**)malloc(bytesInArrayOfPtrsToRows);
+	assert(arrayOfPtrsToRows != NULL);
+
+	double * newRow = NULL;
+	int fileRow=0;
+	for (fileRow=0; fileRow<totalRows; fileRow++) {
+
+		// allocate mem for new row in matrix
+		newRow = (double*)malloc(bytesPerRow);
+		assert(newRow != NULL);
+
+		// place data into the new row
+		int fileCol=0;
+		for (fileCol=0; fileCol<totalCols; fileCol++) {
+			assert(fscanf(fp," %lf\n", &(newRow[fileCol])) == 1);
+		}
+
+		// add new row into array of rows
+		arrayOfPtrsToRows[fileRow] =  newRow;
 	}
-	return newRow;
+	return arrayOfPtrsToRows;
 }
 
 void printMinMaxInEachCol(matrix_t matrix) {
 	
-	double colMin, colMax, matrixEntry;
+	double colMin=DBL_MAX, colMax=-DBL_MAX;
+	int row=0, col=0;
 
-	int row = 0, col=0;
 	for (col=0; col<matrix.cols; col++) {
-		
-		colMin = DBL_MAX, colMax = -DBL_MAX;
 
+		colMin = DBL_MAX, colMax = -DBL_MAX;
 		for (row=0; row<matrix.rows; row++) {
-			matrixEntry = matrix.data[row][col];
-			colMax = matrixEntry > colMax ? matrixEntry : colMax;
-			colMin = matrixEntry < colMin ?  matrixEntry : colMin;
+			colMax = MAX(matrix.data[row][col], colMax);
+			colMin = MIN(matrix.data[row][col], colMin);
 		}
 
 		printf("Column %d: min = %.0f, max = %.0f\n", col+1, colMin, colMax);
@@ -127,7 +129,7 @@ void printMinMaxInEachCol(matrix_t matrix) {
 
 void deleteMatrix(matrix_t * matrix) {
 
-	int fileRow=0;
+	int fileRow = 0;
 	for (fileRow=0; fileRow<matrix->rows; fileRow++) {
 		free(matrix->data[fileRow]);
 	}
